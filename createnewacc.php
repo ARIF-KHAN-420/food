240d1776e0b8fd18f0d9aee2e76d87629febd71b
MD.  ARIF KHAN <45042822+ARIF-KHAN-420@users.noreply.github.com>
2020-08-18 22:41:20 +0600

    Add files via upload
    new food project upload

---
+++ createnewacc.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Getting Started</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="assets/css/styles.min.css">
+</head>
+
+<body>
+    <div c
+    lass="container">
+    <!-- Start: Login-Form-blue-Gradient -->
+    <section>
+        <div class="lgp-hd mx-auto">
+            <h2 style="filter: brightness(200%) ;font-size: 55px;" class="mx-auto"><strong>Food Block</strong></h2>
+            <h5 style="filter: grayscale(100%);" class="mx-auto"><strong>A easy way to get your Food</strong></h5>
+        </div>
+        <div class="container login-cont">
+            <div class="row">
+                <div class="mx-auto login-col col-8"><i class="icon ion-lock-combination"></i>
+                    <form class="login-form" method="post" action="#"> 
+                        <div class="form-group"><input class="form-control form-control-lg lg-frc" type="text" required placeholder="User Name"></div>
+                        <div class="form-group"><input class="form-control form-control-lg lg-frc" type="email" required placeholder="E-mail Address"></div> 
+
+                        <div class="form-group"><input class="form-control form-control-lg lg-frc" type="password" required placeholder="Enter Password"></div>
+                        <div class="form-group"><input class="form-control form-control-lg lg-frc" type="password" required placeholder="Re-type Password"></div>
+                        <div class="form-group">
+                            <button class="btn btn-light btn-lg w-100" type="submit"><strong>Signup</strong></button>
+                        </div>
+                        <div class="d-block mx-auto">
+                        <span class="text-white"><a href="index.php" class="text-light" type="submit"><strong>Login</strong></a></span>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+        <!-- End: Login-Form-blue-Gradient -->
+    </div>
+
+</body>
+
+</html>
